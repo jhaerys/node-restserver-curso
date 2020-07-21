3b8029775e02b25518dec60d473ab430a9dfe1de
Ignacio <ignacio.arriagada.ri@gmail.com>
2020-07-21 17:17:47 -0400

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -17,8 +17,7 @@ let urlDB;
 if (process.env.NODE_ENV === "dev") {
     urlDB = "mongodb://localhost:27017/cafe";
 } else {
-    urlDB =
-        "mongodb+srv://IgnacioDev:Contrasena1109@cluster0.8dgig.mongodb.net/cafeteria";
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
